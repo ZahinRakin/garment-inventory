e3942a94c82ede492c4983bdc8ab66e24d3cbd8b
ZahinRakin <zahinabdullahrakin@gmail.com>
2025-07-10 10:57:13 +0600

    deleted backend folder

--- backend/src/main/java/com/garments/inventory/domain/repositories/PurchaseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.garments.inventory.domain.repositories;
-
-import com.garments.inventory.domain.entities.Purchase;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface PurchaseRepository extends JpaRepository<Purchase, UUID> {
-    // You can add custom query methods here if needed
-}
--- backend/src/main/java/com/garments/inventory/domain/repositories/SupplierRepository.java
+++
@@ -1,9 +0,0 @@
-package com.garments.inventory.domain.repositories;
-
-import com.garments.inventory.domain.entities.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface SupplierRepository extends JpaRepository<Supplier, UUID> {
-    // You can add custom query methods here if needed
-}
