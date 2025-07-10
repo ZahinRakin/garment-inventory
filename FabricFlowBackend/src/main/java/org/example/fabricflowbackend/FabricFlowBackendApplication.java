16e512a964c6a77146a54bea3c2470f755e1b89a
nowsadmunna <bsse1407@iit.du.ac.bd>
2025-07-10 11:18:13 +0600

    fabricflowbackend

---
+++ FabricFlowBackend/src/main/java/org/example/fabricflowbackend/FabricFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.fabricflowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricFlowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FabricFlowBackendApplication.class, args);
+    }
+
+}
---
+++ FabricFlowBackend/src/test/java/org/example/fabricflowbackend/FabricFlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fabricflowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricFlowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
